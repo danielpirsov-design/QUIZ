04d053bb91395afd84b5a9513bf4d50ad534f4f5
Daniel <daniel@gmail.com>
2026-05-23 15:52:18 +0300

    fix ts2308 exports

--- lib/api-zod/src/index.ts
+++ lib/api-zod/src/index.ts
@@ -1,2 +1,16 @@
-export * from "./generated/api"  
-export type * from "./generated/types" 
+export * from "./generated/api"
+
+export type {
+  AiChatBody,
+  AiChatResponse,
+  CreateGameBody,
+  CreateQuizBody,
+ GenerateQuizBody,
+  ImproveQuestionBody,
+  JoinGameBody,
+  LoginBody,
+  RegisterBody,
+  SubmitAnswerBody,
+  UpdateQuestionBody,
+  UpdateQuizBody
+} from "./generated/types"
